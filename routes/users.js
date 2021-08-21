b1fdad6ae59ac08e77da4c783a53377743860eb3
Shimanti <shimanti@gmail.com>
2021-08-20 19:19:07 -0700

    follow request

--- routes/users.js
+++ routes/users.js
@@ -42,13 +42,35 @@ router.delete("/:id", async(req, res) => {
 router.get('/:id', async (req, res) => {
   try{
     const user = await User.findById(req.params.id)
-    res.status(200).json(user)
+    const {password, userId, ...other} = user._doc
+    res.status(200).json(other)
   } catch(err) {
     return res.status(500).json(err)
   }
 })
 
 // follow a user
+// checks to see if user is the same as the follower before updating following and followers
+router.put('/:id/follow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId)
+      if(!user.followers.includes(req.body.userId)){
+        await user.updateOne({$push: { followers: req.body.userId } })
+        await currentUser.updateOne({ $push: { followings: req.body.userId }})
+        res.status(200).json("user has been followed")
+      } else {
+        res.status(403).json("you already follow this user")
+      }
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  } else {
+    res.status(403).json("you can't follow yourself")
+  }
+})
+
 // unfollow a user
 
 module.exports = router
